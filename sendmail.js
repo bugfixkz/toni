ed2249282f96a67903f1e1f28b5580520667c2eb
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-02-08 23:59:10 +0600

    send mail training

--- sendmail.js
+++ sendmail.js
@@ -11,3 +11,17 @@ function validate(){
       message => alert(message)
     );
 }
+
+function validateTrain(){
+    var name = document.getElementById('username').value;
+    var phone = document.getElementById('userphone').value;
+    Email.send({
+        SecureToken : "c0ddc224-b6eb-4f7a-b8d9-93336a9126a8",
+        To : 'admin@astana.toniandguy.kz',
+        From : "request@astana.toniandguy.kz",
+        Subject : "Заявка на обучение с сайта astana.toniandguy.kz",
+        Body : "<b>Имя: </b>" + name + "<br/> <b>Номер телефона: </b>" + phone
+    }).then(
+      message => alert(message)
+    );
+}

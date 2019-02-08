8af08103158b9717b616db3e5ee024e5f3a7fe3b
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-02-08 19:58:16 +0600

    index send mail

---
+++ sendmail.js
@@ -0,0 +1,13 @@
+function validate(){
+    var name = document.getElementById('username').value;
+    var phone = document.getElementById('userphone').value;
+    Email.send({
+        SecureToken : "c0ddc224-b6eb-4f7a-b8d9-93336a9126a8",
+        To : 'admin@astana.toniandguy.kz',
+        From : "request@astana.toniandguy.kz",
+        Subject : "Заявка с сайта astana.toniandguy.kz",
+        Body : "<b>Имя: </b>" + name + "<br/> <b>Номер телефона: </b>" + phone
+    }).then(
+      message => alert(message)
+    );
+}

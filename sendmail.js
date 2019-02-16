c63c24709db455adccc44ef82587d175421034cd
Sultan Kadyrkesh <sultankadyr@outlook.com>
2019-02-16 15:38:18 +0600

    admin vacancies

--- sendmail.js
+++ sendmail.js
@@ -25,3 +25,17 @@ function validateTrain(){
       message => alert(message)
     );
 }
+
+function validateVacancies(){
+    var name = document.getElementById('username').value;
+    var phone = document.getElementById('userphone').value;
+    Email.send({
+        SecureToken : "c0ddc224-b6eb-4f7a-b8d9-93336a9126a8",
+        To : 'hello@toniandguy.kz',
+        From : "request@astana.toniandguy.kz",
+        Subject : "Заявка на вакансию с сайта astana.toniandguy.kz",
+        Body : "<b>Имя: </b>" + name + "<br/> <b>Номер телефона: </b>" + phone
+    }).then(
+      message => alert(message)
+    );
+}
